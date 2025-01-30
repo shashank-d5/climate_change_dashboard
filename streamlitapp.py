07b599f999d0828667bfb5886ea190a4eef8eed7
shashank-d5 <157282114+shashank-d5@users.noreply.github.com>
2025-01-30 23:52:20 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import plotly.graph_objects as go
 
 # Load the dataset
-df = pd.read_csv(r'C:\Users\home\Downloads\climate_change_dataset.csv')
+df = pd.read_csv(r'https://drive.google.com/file/d/1ouidvn7aisQhRT_Wmf-xlgixMPAGK7J3/view?usp=drive_link')
 
 # Data preprocessing
 df['Year'] = pd.to_numeric(df['Year'], errors='coerce').astype('Int64')
